2bbf6f6501cc07dd01c38be8d647c76b8bb219bf
vstartech11 <11211024@student.itk.ac.id>
2022-06-30 14:59:10 +0800

    penambahan form hutang

--- Toko Rusber/formDHutang.Designer.vb
+++ Toko Rusber/formDHutang.Designer.vb
@@ -22,35 +22,128 @@ Partial Class formDHutang
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
-        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.dgvHutang = New System.Windows.Forms.DataGridView()
+        Me.txtbxCari = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnCari = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.lblNama = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.lblJHutang = New System.Windows.Forms.Label()
+        Me.btnHapus = New System.Windows.Forms.Button()
+        CType(Me.dgvHutang, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
-        'DataGridView1
+        'dgvHutang
         '
-        Me.DataGridView1.AllowUserToAddRows = False
-        Me.DataGridView1.AllowUserToDeleteRows = False
-        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridView1.Location = New System.Drawing.Point(73, 78)
-        Me.DataGridView1.Name = "DataGridView1"
-        Me.DataGridView1.ReadOnly = True
-        Me.DataGridView1.RowHeadersWidth = 51
-        Me.DataGridView1.RowTemplate.Height = 24
-        Me.DataGridView1.Size = New System.Drawing.Size(810, 180)
-        Me.DataGridView1.TabIndex = 0
+        Me.dgvHutang.AllowUserToAddRows = False
+        Me.dgvHutang.AllowUserToDeleteRows = False
+        Me.dgvHutang.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvHutang.Location = New System.Drawing.Point(73, 78)
+        Me.dgvHutang.Name = "dgvHutang"
+        Me.dgvHutang.ReadOnly = True
+        Me.dgvHutang.RowHeadersWidth = 51
+        Me.dgvHutang.RowTemplate.Height = 24
+        Me.dgvHutang.Size = New System.Drawing.Size(810, 180)
+        Me.dgvHutang.TabIndex = 0
+        '
+        'txtbxCari
+        '
+        Me.txtbxCari.Location = New System.Drawing.Point(187, 277)
+        Me.txtbxCari.Name = "txtbxCari"
+        Me.txtbxCari.Size = New System.Drawing.Size(149, 22)
+        Me.txtbxCari.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(84, 280)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(71, 16)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Cari Nama"
+        '
+        'btnCari
+        '
+        Me.btnCari.Location = New System.Drawing.Point(370, 272)
+        Me.btnCari.Name = "btnCari"
+        Me.btnCari.Size = New System.Drawing.Size(66, 33)
+        Me.btnCari.TabIndex = 3
+        Me.btnCari.Text = "Cari"
+        Me.btnCari.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(84, 323)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(98, 16)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Nama Anggota"
+        '
+        'lblNama
+        '
+        Me.lblNama.AutoSize = True
+        Me.lblNama.Location = New System.Drawing.Point(211, 323)
+        Me.lblNama.Name = "lblNama"
+        Me.lblNama.Size = New System.Drawing.Size(0, 16)
+        Me.lblNama.TabIndex = 2
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(84, 360)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(96, 16)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Jumlah Hutang"
+        '
+        'lblJHutang
+        '
+        Me.lblJHutang.AutoSize = True
+        Me.lblJHutang.Location = New System.Drawing.Point(211, 360)
+        Me.lblJHutang.Name = "lblJHutang"
+        Me.lblJHutang.Size = New System.Drawing.Size(0, 16)
+        Me.lblJHutang.TabIndex = 2
+        '
+        'btnHapus
+        '
+        Me.btnHapus.Location = New System.Drawing.Point(370, 337)
+        Me.btnHapus.Name = "btnHapus"
+        Me.btnHapus.Size = New System.Drawing.Size(112, 62)
+        Me.btnHapus.TabIndex = 3
+        Me.btnHapus.Text = "HAPUS"
+        Me.btnHapus.UseVisualStyleBackColor = True
         '
         'formDHutang
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(966, 484)
-        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.btnHapus)
+        Me.Controls.Add(Me.btnCari)
+        Me.Controls.Add(Me.lblJHutang)
+        Me.Controls.Add(Me.lblNama)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtbxCari)
+        Me.Controls.Add(Me.dgvHutang)
         Me.Name = "formDHutang"
         Me.Text = "formDHutang"
-        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.dgvHutang, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
-    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents dgvHutang As DataGridView
+    Friend WithEvents txtbxCari As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents btnCari As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents lblNama As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents lblJHutang As Label
+    Friend WithEvents btnHapus As Button
 End Class
--- Toko Rusber/formDHutang.vb
+++ Toko Rusber/formDHutang.vb
@@ -1,3 +1,65 @@
-﻿Public Class formDHutang
+﻿Imports System.Data.SqlClient
+Public Class formDHutang
+    Dim noAnggota As String
+    Sub awal()
+        Call getData()
+        txtbxCari.Text = vbNullString
+        txtbxCari.Enabled = True
+        btnCari.Enabled = True
+        btnHapus.Enabled = False
+        lblJHutang.Text = vbNullString
+        lblNama.Text = vbNullString
+        noAnggota = vbNullString
 
+    End Sub
+    Sub getData()
+        Call koneksi()
+        Da = New SqlDataAdapter("select tblAnggota.nama,tblHutang.tHutang,tblHutang.tglHutang from tblHutang join tblAnggota on tblAnggota.noAnggota=tblHutang.noAnggota", Conn)
+        Ds = New DataSet
+        Ds.Clear()
+        Da.Fill(Ds, "tblHutang")
+        dgvHutang.DataSource = (Ds.Tables("tblHutang"))
+        Conn.Close()
+    End Sub
+    Private Sub btnCari_Click(sender As Object, e As EventArgs) Handles btnCari.Click
+        If txtbxCari.Text = vbNullString Or txtbxCari.Text = " " Then
+            Call getData()
+        Else
+            Call koneksi()
+            Da = New SqlDataAdapter("select tblAnggota.nama,tblHutang.tHutang,tblHutang.tglHutang from tblHutang join tblAnggota on tblAnggota.noAnggota=tblHutang.noAnggota where tblAnggota.noAnggota like '%" & txtbxCari.Text & "%' OR tblHutang.tglHutang like '%" & txtbxCari.Text & "%'", Conn)
+            Ds = New DataSet
+            Ds.Clear()
+            Da.Fill(Ds, "tblHutang")
+            dgvHutang.DataSource = (Ds.Tables("tblHutang"))
+            Conn.Close()
+        End If
+    End Sub
+
+    Private Sub formDHutang_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Call awal()
+    End Sub
+
+    Private Sub dgvHutang_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles dgvHutang.CellClick
+        btnCari.Enabled = False
+        txtbxCari.Enabled = False
+        Call koneksi()
+        Cmd = New SqlCommand("Select noAnggota from tblAnggota where nama='" & dgvHutang.Rows(e.RowIndex).Cells(0).Value.ToString & "'", Conn)
+        Rd = Cmd.ExecuteReader
+        If Rd.Read Then
+            noAnggota = Rd.Item(0).ToString
+            lblNama.Text = dgvHutang.Rows(e.RowIndex).Cells(0).Value.ToString
+            lblJHutang.Text = dgvHutang.Rows(e.RowIndex).Cells(1).Value.ToString
+            btnHapus.enabled = True
+        End If
+    End Sub
+
+    Private Sub btnHapus_Click(sender As Object, e As EventArgs) Handles btnHapus.Click
+        If (MsgBox("Yakin mereset hutang saudara " + lblNama.Text + " sejumlah " + lblJHutang.Text, MsgBoxStyle.YesNo) = MsgBoxResult.Yes) Then
+            Call koneksi()
+            Cmd = New SqlCommand("update tblHutang set tHutang='0',tglHutang='" & Date.Now.ToString("yyyy-MM-dd") & "' where noAnggota='" & noAnggota & "'", Conn)
+            Cmd.ExecuteNonQuery()
+            MsgBox("Sukses mereset hutang, trimss")
+            Call awal()
+        End If
+    End Sub
 End Class
